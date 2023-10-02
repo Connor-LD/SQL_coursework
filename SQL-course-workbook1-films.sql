35d34854ab2a274fc37d01c4f1b009460aef5ca9
Connor LeDrew <122570322+Connor-LD@users.noreply.github.com>
2023-10-02 12:35:15 -0700

    Add files via upload
    Completed Day 8 Test.  Result 13/14, considered outstanding within the course.  Incorrect answer #9 due to misinterpretation of question.  Details within.

--- SQL-course-workbook1-films.sql
+++ SQL-course-workbook1-films.sql
@@ -115,7 +115,8 @@ use alias 'no_of_movies'
 -- SUBSTRING(email FROM POSITION('.' IN email)+1 FOR (POSITION('@' IN email)-POSITION('.' IN email))-1) AS last_name
 -- FROM customer
 
---Challenge: Create anonymized version of email where initials are visible and rest is '***'.  pt2: last letter of first stanza then first initial of last stanza
+--Challenge: Create anonymized version of email where initials are visible and rest is '***'.  
+-- pt2: last letter of first stanza then first initial of last stanza
 -- pt1.
 -- SELECT
 -- email,
@@ -237,7 +238,462 @@ LEFT JOIN country
 	
 	
 
+-- *** Day 7: SQL: Unions & subqueries
+-- example: union
+SELECT first_name, 'actor' AS origin FROM actor
+UNION ALL
+SELECT first_name, 'customer' FROM customer
+UNION
+SELECT UPPER(first_name) AS first_name, 'Staff' FROM staff
+ORDER BY 2;
 
--- Question 3:
--- Which title has GEORGE LINTON rented the most often?
--- Answer: CADDYSHACK JEDI - 3 times.
+-- example: subqueries
+SELECT *
+FROM payment
+WHERE customer_id IN (SELECT customer_id FROM customer 
+					  WHERE last_name LIKE 'A%');
+
+-- Select films where length is greater than average
+SELECT *
+FROM film
+WHERE length > (SELECT AVG(length) FROM film);
+
+-- Return all films that are in inventory for store 2 more than 3 times; RETURN film_id, count of inventory and title
+
+SELECT *
+FROM film
+WHERE film_id IN
+(SELECT film_id
+FROM inventory
+WHERE store_id = 2
+GROUP BY film_id
+HAVING count(film_id) > 3);
+
+-- Q: Subqueries in WHERE: return customer name and email iff spend > $30
+SELECT first_name, email
+FROM customer
+WHERE customer_id IN 
+	(SELECT customer_id
+	 FROM payment
+	 GROUP BY customer_id
+	 HAVING SUM(amount) > 30);
+
+-- Q: Subqueries in WHERE: return customer fname, lname iff California && spend > $100
+SELECT first_name,last_name
+FROM customer
+WHERE address_id IN (
+	SELECT address_id
+	FROM address
+	WHERE district = 'California'
+	)
+	AND
+	customer_id IN (
+	SELECT customer_id
+	FROM payment
+	GROUP BY customer_id
+	HAVING SUM(amount) > 100
+	);
+
+SELECT first_name,last_name
+FROM customer
+WHERE customer_id IN (
+	SELECT customer_id
+	FROM customer c
+	LEFT JOIN address a
+	ON c.address_id = a.address_id
+	WHERE district = 'California'
+	)
+	AND
+	customer_id IN (
+	SELECT customer_id
+	FROM payment
+	GROUP BY customer_id
+	HAVING SUM(amount) > 100
+	);
+
+
+
+
+-- Q: Waht is the avg daily spend
+SELECT AVG(daily_amt)
+FROM
+(SELECT SUM(amount) as daily_amt, DATE(payment_date) AS day
+FROM payment
+GROUP BY day) as subquery;
+
+-- Correlated Subquery in WHERE
+-- Ex.  Show payments that only have the highest payment per customer
+SELECT *
+FROM payment p1
+WHERE amount = (SELECT MAX(amount) FROM payment p2 WHERE p1.customer_id = p2.customer_id)
+ORDER BY customer_id;
+
+-- Q: Show movies with highest length in their category, including rating also
+-- Problem:  How to get 1 entry for each rating when multiple movies tite for length
+SELECT film_id, title, length, rating
+FROM film f1
+WHERE length = (SELECT MAX(length) FROM film f2 WHERE f1.rating = f2.rating);
+
+-- Correlated subquery in SELECT
+-- Q: Show all payments plus the count and total amount per customer
+
+SELECT *,
+(SELECT COUNT(customer_id) FROM payment p2 WHERE p1.customer_id=p2.customer_id) as count,
+(SELECT SUM(amount) FROM payment p3 WHERE p1.customer_id=p3.customer_id) as sum
+FROM payment p1
+ORDER BY count;
+
+-- ***Day 7: Challenges:
+-- 1. Show only films with highest replacement cost in their category + avg replacement_cost
+SELECT film_id, title, replacement_cost, rating,
+ROUND((SELECT AVG(replacement_cost) FROM film f3 WHERE f1.rating=f3.rating),2) as Avg_replacement_cost
+FROM film f1
+WHERE replacement_cost = (SELECT MAX(replacement_cost) FROM film f2 WHERE f1.rating = f2.rating)
+ORDER BY rating;
+
+
+-- 2. Show highest payment, including payment_id, for each customer (using joins show first name)
+SELECT first_name, amount, payment_id
+FROM payment p1
+INNER JOIN customer c
+	ON c.customer_id = p1.customer_id
+WHERE amount = (SELECT MAX(amount) FROM payment p2 
+				WHERE p1.customer_id = p2.customer_id);
+	
+-- SELECT * FROM payment
+
+-- 2. Bonus: how ot solve if didn't need payment_id
+SELECT first_name, MAX(amount)
+FROM payment p1
+INNER JOIN customer c
+	ON c.customer_id = p1.customer_id
+GROUP BY c.customer_id;
+
+
+-- *** DAY 8: SQL: PROJECT/Portfolio problems  (goal: 10/14)
+/*
+Question 1:
+Level: Simple
+Topic: DISTINCT
+Task: Create a list of all the different (distinct) replacement costs of the films.
+Question: What's the lowest replacement cost?
+Answer: 9.99
+*/
+SELECT DISTINCT replacement_cost
+FROM film
+ORDER BY replacement_cost ASC
+LIMIT 1;
+
+/*
+Question 2:
+Level: Moderate
+Topic: CASE + GROUP BY
+Task: Write a query that gives an overview of how many films have replacements costs in the following cost ranges
+low: 9.99 - 19.99
+medium: 20.00 - 24.99
+high: 25.00 - 29.99
+Question: How many films have a replacement cost in the "low" group?
+Answer: 514
+*/
+SELECT COUNT(*),
+(CASE
+WHEN replacement_cost >= 9.99 AND replacement_cost <=19.99 THEN 'low'
+WHEN replacement_cost >=20 AND replacement_cost <=24.99 THEN 'medium'
+WHEN replacement_cost >=25 AND replacement_cost <=29.99 THEN 'high'
+ELSE NULL
+END) AS category
+FROM film
+GROUP BY category;
+
+
+/*
+Question 3:
+Level: Moderate
+Topic: JOIN
+Task: Create a list of the film titles including their title, length, and category name ordered descendingly by length. 
+Filter the results to only the movies in the category 'Drama' or 'Sports'.
+Question: In which category is the longest film and how long is it?
+Answer: Sports and 184
+*/
+-- SELECT title, length, 
+-- FROM film
+
+SELECT f.title,f.length,c.name
+FROM film f
+LEFT JOIN film_category fc
+	ON f.film_id = fc.film_id
+LEFT JOIN category c
+	ON c.category_id=fc.category_id
+WHERE name IN ('Drama','Sports')
+ORDER BY length DESC;
+
+-- SELECT * FROM category
+/*
+Question 4:
+Level: Moderate
+Topic: JOIN & GROUP BY
+Task: Create an overview of how many movies (titles) there are in each category (name).
+Question: Which category (name) is the most common among the films?
+Answer: Sports with 74 titles
+*/
+SELECT COUNT(*),c.name
+FROM film f
+LEFT JOIN film_category fc
+	ON f.film_id=fc.film_id
+LEFT JOIN category c
+	ON fc.category_id=c.category_id
+GROUP BY c.name
+ORDER BY count DESC;
+
+
+
+
+/*
+Question 5:
+Level: Moderate
+Topic: JOIN & GROUP BY
+Task: Create an overview of the actors' first and last names and in how many movies they appear in.
+Question: Which actor is part of most movies??
+Answer: Susan Davis with 54 movies
+*/
+SELECT COUNT(*), a.first_name, a.last_name
+FROM actor a
+LEFT JOIN film_actor fa
+	ON a.actor_id=fa.actor_id
+GROUP BY a.actor_id
+ORDER BY count DESC;
+-- Note: Answer is different.  Confident in the structure of my answer; updates have been made to dataset
+
+
+/*
+Question 6:
+Level: Moderate
+Topic: LEFT JOIN & FILTERING
+Task: Create an overview of the addresses that are not associated to any customer.
+Question: How many addresses are that?
+Answer: 4
+*/
+SELECT a.address_id, a.address, a.district
+FROM address a
+LEFT JOIN customer c
+	ON a.address_id=c.address_id
+WHERE c.address_id IS NULL;
+
+/*
+Question 7:
+Level: Moderate
+Topic: JOIN & GROUP BY
+Task: Create an overview of the cities and how much sales (sum of amount) have occurred there.
+Question: Which city has the most sales?
+Answer: Cape Coral with a total amount of 221.55
+*/
+SELECT ci.city,SUM(amount)
+FROM city ci
+LEFT JOIN address a
+	ON ci.city_id = a.city_id
+LEFT JOIN customer cu
+	ON cu.address_id = a.address_id
+LEFT JOIN payment p
+	ON p.customer_id = cu.customer_id
+GROUP BY ci.city
+ORDER BY sum DESC;
+
+-- table map:: payment: amount and customer_id  > customer: customer_id, address_id > address: address_id, city_id > city: city, city_id
+
+/*
+Question 8:
+Level: Moderate to difficult
+Topic: JOIN & GROUP BY
+Task: Create an overview of the revenue (sum of amount) grouped by a column in the format "country, city".
+Question: Which country, city has the least sales?
+Answer: United States, Tallahassee with a total amount of 50.85.
+*/
+
+SELECT (co.country || ', ' || ci.city) as "country, city", SUM(amount) AS sum
+FROM city ci
+FULL JOIN country co
+	ON ci.country_id=co.country_id
+LEFT JOIN address a
+	ON ci.city_id = a.city_id
+LEFT JOIN customer cu
+	ON cu.address_id = a.address_id
+LEFT JOIN payment p
+	ON p.customer_id = cu.customer_id
+-- WHERE sum IS NOT NULL
+GROUP BY "country, city"
+ORDER BY sum ASC;
+
+
+/*
+Question 9:
+Level: Difficult
+Topic: Uncorrelated subquery
+Task: Create a list with the average of the sales amount each staff_id has per customer.
+Question: Which staff_id makes on average more revenue per customer?
+Answer: staff_id 2 with an average revenue of 56.64 per customer.
+*/
+SELECT * FROM payment;
+
+-- First incorrect answer:  misinterpreted question as avg transaction amount by customer + staff id, which doesn't require a subquery. 
+-- After reviewing solution it was clear they wanted each customers total spend by staff id, and an average taken of each customers total spend.
+SELECT
+staff_id,
+ROUND(AVG(total),2)
+FROM (
+	SELECT
+	staff_id, customer_id, SUM(amount) as total
+	FROM payment
+	GROUP BY staff_id, customer_id
+	ORDER BY customer_id
+	) subquery
+GROUP BY staff_id
+;
+
+
+
+/*
+Question 10:
+Level: Difficult to very difficult
+Topic: EXTRACT + Uncorrelated subquery
+Task: Create a query that shows average daily revenue of all Sundays.
+Question: What is the daily average revenue of all Sundays?
+Answer: 1410.65
+*/
+-- Extract DOW, Sunday = 0.
+
+SELECT ROUND(AVG(revenue),2)
+FROM (
+	SELECT sum(amount) as revenue, DATE(payment_date) as date
+	FROM payment
+	WHERE EXTRACT(DOW from payment_date) = 0
+	GROUP BY DATE(payment_date)
+	) subquery
+	;
+
+
+/*
+Question 11:
+Level: Difficult to very difficult
+Topic: Correlated subquery
+Task: Create a list of movies - with their length and their replacement cost - 
+		that are longer than the average length in each replacement cost group.
+Question: Which two movies are the shortest on that list and how long are they?
+Answer: CELEBRITY HORN and SEATTLE EXPECTATIONS with 110 minutes.
+*/
+SELECT * FROM film;
+
+SELECT title, length, replacement_cost
+FROM film f1
+WHERE length > (SELECT AVG(length) FROM film f2 WHERE f1.replacement_cost=f2.replacement_cost)
+ORDER BY length;
+
+
+
+/*
+Question 12:
+Level: Very difficult
+Topic: Uncorrelated subquery
+Task: Create a list that shows the "average customer lifetime value" grouped by the different districts.
+Example:
+If there are two customers in "District 1" where one customer has a total (lifetime) spent of $1000 
+	and the second customer has a total spent of $2000 
+	then the "average customer lifetime spent" in this district is $1500.
+So, first, you need to calculate the total per customer and then the average of these totals per district.
+Question: Which district has the highest average customer lifetime value?
+Answer: Saint-Denis with an average customer lifetime value of 216.54.
+*/
+SELECT * FROM address;
+-- join map:: payment: customer_id > customer: address_id > address: district
+-- no possibility of customer being in 2 districts
+
+SELECT district, ROUND(AVG(customer_lifetime_value),2) as avg_customer_lifetime_spend
+FROM
+	(
+	SELECT p.customer_id, a.district,SUM(amount) as customer_lifetime_value
+	FROM payment p 
+	LEFT JOIN customer c
+		ON c.customer_id=p.customer_id
+	LEFT JOIN address a
+		ON a.address_id=c.address_id
+	GROUP BY p.customer_id, a.district
+	) as sub1
+GROUP BY district
+ORDER BY avg_customer_lifetime_spend DESC
+;
+
+
+
+/*
+Question 13:
+Level: Very difficult
+Topic: Correlated query
+Task: Create a list that shows all payments including the payment_id, amount, and the film category (name) 
+	plus the total amount that was made in this category. Order the results ascendingly by the category (name) 
+	and as second order criterion by the payment_id ascendingly.
+Question: What is the total revenue of the category 'Action' 
+	and what is the lowest payment_id in that category 'Action'?
+Answer: Total revenue in the category 'Action' is 4375.85 and the lowest payment_id in that category is 16055.
+*/
+-- map:: payment (rental_id) > rental (inventory_id) > inventory (film_id) > film_cat (category_id) > category (name)
+SELECT title, payment_id, amount, c.name,
+(SELECT SUM(amount) FROM payment p
+					LEFT JOIN rental r ON r.rental_id = p.rental_id
+					LEFT JOIN inventory i ON i.inventory_id = r.inventory_id
+					LEFT JOIN film f ON f.film_id = i.film_id
+					LEFT JOIN film_category fc ON fc.film_id = f.film_id
+					LEFT JOIN category c ON c.category_id = fc.category_id
+					WHERE c1.name=c.name)
+
+FROM payment p
+LEFT JOIN rental r ON r.rental_id = p.rental_id
+LEFT JOIN inventory i ON i.inventory_id = r.inventory_id
+LEFT JOIN film f ON f.film_id = i.film_id
+LEFT JOIN film_category fc ON fc.film_id = f.film_id
+LEFT JOIN category c ON c.category_id = fc.category_id
+ORDER BY name;
+
+--  Later optimizated by partitions  (90msec vs 25sec!!)
+SELECT c.name, p.payment_id, p.amount, SUM(p.amount) OVER(PARTITION BY c.name) AS category_total
+FROM payment p
+LEFT JOIN rental r ON r.rental_id = p.rental_id
+LEFT JOIN inventory i ON i.inventory_id = r.inventory_id
+LEFT JOIN film f ON f.film_id = i.film_id
+LEFT JOIN film_category fc ON fc.film_id = f.film_id
+LEFT JOIN category c ON c.category_id = fc.category_id
+ORDER BY c.name, p.payment_id;
+
+
+
+/*
+Bonus question 14:
+Level: Extremely difficult
+Topic: Correlated and uncorrelated subqueries (nested)
+Task: Create a list with the top overall revenue of a film title (sum of amount per title) for each category (name).
+Question: Which is the top-performing film in the animation category?
+Answer: DOGMA FAMILY with 178.70.
+*/
+SELECT title,name,SUM(amount) as total
+FROM payment p
+LEFT JOIN rental r ON r.rental_id=p.rental_id
+LEFT JOIN inventory i ON i.inventory_id=r.inventory_id
+LEFT JOIN film f ON f.film_id=i.film_id
+LEFT JOIN film_category fc ON fc.film_id=f.film_id
+LEFT JOIN category c ON c.category_id=fc.category_id
+GROUP BY name,title
+HAVING SUM(amount) =
+	(SELECT MAX(total)
+	FROM (
+			SELECT title,name,SUM(amount) as total
+			FROM payment p
+			LEFT JOIN rental r ON r.rental_id=p.rental_id
+			LEFT JOIN inventory i ON i.inventory_id=r.inventory_id
+			LEFT JOIN film f ON f.film_id=i.film_id
+			LEFT JOIN film_category fc ON fc.film_id=f.film_id
+			LEFT JOIN category c ON c.category_id=fc.category_id
+			GROUP BY name,title
+		) as sub
+	 WHERE c.name=sub.name)
+ORDER BY total DESC
+;
+
+-- Score (13/14): Incorrect answers: #9 (misinterpreted question)
--- SQL-course-workbook2-airline.sql
+++ SQL-course-workbook2-airline.sql
@@ -1,4 +1,44 @@
-SQL-course-workbook2
-
 -- *** SQL Masterclass: Day 6: Joins
 -- airline: how many people choose seats in business, economy, comfort; using seats and boarding_passes
+
+SELECT
+*
+FROM boarding_passes;
+
+SELECT
+COUNT(*),
+fare_conditions
+FROM boarding_passes AS b
+INNER JOIN seats AS s
+ON b.seat_no = s.seat_no
+GROUP BY fare_conditions;
+
+
+-- -- Q: Find tickets without a related boarding pass
+-- SELECT
+-- *
+-- FROM tickets t
+-- FULL OUTER JOIN boarding_passes b
+-- ON t.ticket_no = b.ticket_no
+-- WHERE b.flight_id is null
+
+-- Find which line (A, B, H, etc) has been chosen the most frequently using left outer join
+-- unnecessary join,just need boarding passes
+SELECT
+RIGHT(s.seat_no,1), COUNT(*)
+FROM seats s
+LEFT OUTER JOIN boarding_passes b
+ON s.seat_no = b.seat_no
+GROUP BY RIGHT(s.seat_no,1)
+ORDER BY COUNT(*) DESC;
+
+
+-- Q:  Find avg price for each seat after joining tables (multiple conditions)
+SELECT
+seat_no,ROUND(AVG(amount),2) AS avg_cost
+FROM boarding_passes b
+LEFT JOIN ticket_flights t
+ON b.ticket_no = t.ticket_no
+	AND b.flight_id = t.flight_id
+GROUP BY seat_no
+ORDER BY avg_cost DESC;
